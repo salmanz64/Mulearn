366997bc28ddadd4588caec10ee4f5d67f1cfcf2
Salman T <salmannoushad003@gmail.com>
2024-12-26 22:40:51 +0530

    changes

--- Web development/Level-4/Task-6-TicTacToe/src/square.jsx
+++ Web development/Level-4/Task-6-TicTacToe/src/square.jsx
@@ -6,12 +6,18 @@ import './styles.css'
   return <button className="square" onClick={onSquareClick}>{value}</button>
     
   }
+
+
 export default function Board(){
 
   const [squares,setsquares]= useState(Array(9).fill(null))
   const [xisNext,setXisNext] = useState(true)
 
   function handleClick(i){
+    if(squares[i] || calculateWinner(squares)){
+      return ;
+    }
+
     const nextSquares = squares.slice();
     if(xisNext){
       nextSquares[i] = "X";
@@ -23,8 +29,18 @@ export default function Board(){
     setXisNext(!xisNext);
   }
 
+
+  let winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xisNext ? "X" : "O");
+  }
+
   return (
     <>
+    <div className="status">{status}</div>
      <div className='board-row'>
     <Square value={squares[0]} onSquareClick={()=>handleClick(0)}/>
     <Square value={squares[1]} onSquareClick={()=>handleClick(1)}/>
@@ -43,4 +59,24 @@ export default function Board(){
     
     </>
   )
+}
+function calculateWinner(squares){
+  const lines = [
+    [0, 1, 2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a,b,c] = lines[i];
+    if (squares[a] === squares[b] && squares[a] === squares[c] && squares[a]){
+        return squares[a];
+    }
+  }
+  return null;
 }
